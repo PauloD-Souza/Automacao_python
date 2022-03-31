7b8e0ef957588c7826a62aa4d74cde43df92dc5b
PauloD-Souza <86973749+PauloD-Souza@users.noreply.github.com>
2022-03-31 20:39:56 -0300

    Automação

--- Relatatório_Email/Classe_Principal.py
+++ Relatatório_Email/Classe_Principal.py
@@ -2,44 +2,47 @@ import time
 import pyautogui #Importando o pyautogui        
 import pyperclip #Importando o pyperclip
 import pandas as pd
-pyautogui.PAUSE = 2 #Pausa de 1 segundo
+pyautogui.PAUSE = 2 #Pausa de 1 segundohttps://drive.google.com/drive/folders/1e79icNUtL7k50wTH-c5Pu3nCl3jSiycn
 pyautogui.press("win") #Comando para pressionar a tecla win
-pyautogui.write("chrome") #Comando para escrever o no chrome
+pyautogui.write("edge") #Comando para escrever na barra de pesquisa
 pyautogui.press("enter") #Comando para pressionar a tecla enter
-pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga") #Comando para copiar a url
+time.sleep(3)
+pyperclip.copy("https://drive.google.com/drive/folders/1e79icNUtL7k50wTH-c5Pu3nCl3jSiycn") #Comando para copiar a url
 pyautogui.hotkey("ctrl","v") #Comando para colar o nosso link
 pyautogui.press("enter") #Comando para pressionar a tecla enterhttps://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga
 time.sleep(4)
-pyautogui.click(x=476, y=331, clicks=2)# Comando para dar 2 click na tela, pyautogui.position, é o comando para saber a posição do mouse
+pyautogui.click(x=1617, y=414, clicks=2)# Comando para dar 2 click na tela, pyautogui.position, é o comando para saber a posição do mouse
 time.sleep(3)
-pyautogui.click(x=425, y=395, clicks = 1)# Comando para dar 1 click na tela, pyautogui.position, é o comando para saber a posição do mouse
+pyautogui.click(x=1623, y=327, clicks = 2)# Comando para dar 1 click na tela, pyautogui.position, é o comando para saber a posição do mouse
 time.sleep(3)
-pyautogui.click(x=1661, y=205, clicks =1)# Comando para dar 1 click na tela, pyautogui.position, é o comando para saber a posição do mouse
+pyautogui.click(x=483, y=347, clicks =2)# Comando para dar 1 click na tela, pyautogui.position, é o comando para saber a posição do mouse
 time.sleep(3)
-pyautogui.click(x=1409, y=739, clicks = 1)# Comando para dar 1 click na tela, pyautogui.position, é o comando para saber a posição do mouse
-time.sleep(6)
-tabela = pd.read_excel(r"C:\Users\paulo\Downloads\Vendas - Dez.xlsx") #Lendo a tabela na pagina download
-faturamento = tabela["Valor Final"].sum()#vendo o faturamento e somando
-quantidade = tabela["Quantidade"].sum() #vendo a quantidade elementos na tabela e somando
+pyautogui.click(x=483, y=347, clicks =2)
+time.sleep(1)
+pyautogui.click(x=775, y=340, clicks = 2)# Comando para dar 1 click na tela, pyautogui.position, é o comando para saber a posição do mouse
+time.sleep(3)
+pyautogui.click(x= 1116, y = 504, button='right')
+time.sleep(1)
+pyautogui.click (x = 1119, y = 619, clicks= 1)
+pyautogui.click (x = 812, y = 61, clicks= 1)
+pyautogui.write("projeto")
+pyautogui.press("enter")
+time.sleep(2)
+pyautogui.click(x = 516, y = 229, clicks = 2)
+time.sleep(10)
 pyautogui.hotkey("ctrl","t")#Comando para ultilizar o atalho
-pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")#Comando para copiar a url
+pyperclip.copy("https://web.whatsapp.com/")#Comando para copiar a url
 pyautogui.hotkey("ctrl","v") #Comando para colar o nosso link
-pyautogui.press("enter")#Comando para pressionar a tecla enter
-time.sleep(4)
-pyautogui.click(x=110, y=234, clicks=1)# Comando para dar 1 click na tela, pyautogui.position, é o comando para saber a posição do mouse
+pyautogui.press("enter")#Comando para pressionar a tecla enterchrome
+time.sleep(7)
+pyautogui.click(x=457, y=215, clicks=1)# Comando para dar 1 click na tela, pyautogui.position, é o comando para saber a posição do mouse
 time.sleep(2)
-pyautogui.write("paulodaniel925@gmail.com")#Comando para digitar o email
-time.sleep(2)
+pyautogui.write("max")#Comando para digitar o email
+time.sleep(1)
 pyautogui.press("enter") #comando para pressionar a tecla enter
 time.sleep(2)
-pyautogui.press("tab")# Comando para pular pra a proxima instrução
-pyautogui.write("Faturamento da empresa")#Escrever no assunto do texto
-time.sleep(2)
-pyautogui.press("tab")# Comando para pular pra a proxima instrução
-texto =f""" Prezados, Bom dia 
-        O faturamento de ontem foi de:R${faturamento:,.2f}
-        A quantidade de produtos vendido foi de: R${quantidade:,.2f}"""
-pyperclip.copy(texto)
-pyautogui.hotkey("ctrl","v")
-time.sleep(4)
-pyautogui.hotkey("ctrl","enter")#Código para enviar o email
+pyautogui.write("O video esta sendo upado para o driver, em 5 minutos você pode ir olhar")
+time.sleep(1)
+pyautogui.press("enter")
+time.sleep(1)
+pyautogui.click(x = 1918, y = 6, clicks= 1)
